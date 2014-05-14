4b50c7f49f19f3c8241b807408f60648b761a215
Anupong Chomtapin <anupong@Anupongs-MacBook-Pro.local>
2014-05-14 16:12:03 +0700

    commit Captcha Class

--- Captcha.php
+++ Captcha.php
@@ -1,17 +1,20 @@
 <?php
-function numberToString($pattern, $leftOperand, $operator, $rightOperand){
-	$numberWord = array('Zero','One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine');
-	$operatorSymbol = array('', '+', '*', '-');
-	$result = calculate($leftOperand, $operator, $rightOperand);
-	$leftOperand = ($pattern == 1) ?  $numberWord[$leftOperand] : $leftOperand;
-	$rightOperand = ($pattern == 2) ? $numberWord[$rightOperand] : $rightOperand;
-	return sprintf('%s %s %s = %d', $leftOperand, $operatorSymbol[$operator], $rightOperand, $result);
-}
+class Captcha {
+	private $numberWord = array('Zero','One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine');
+	private $symbol = array('', '+', '*', '-');
 
-function calculate($leftOperand, $operator, $rightOperand) {
-	switch ($operator) {
-		case 1: return $leftOperand + $rightOperand;
-		case 2: return $leftOperand * $rightOperand;
-		case 3: return $leftOperand - $rightOperand;
+	public function getCaptcha($pattern, $leftOperand, $operator, $rightOperand) {
+		$result = $this->calculate($operator, $leftOperand, $rightOperand);
+		$leftOperand = ($pattern == 1) ?  $this->numberWord[$leftOperand] : $leftOperand;
+		$rightOperand = ($pattern == 2) ? $this->numberWord[$rightOperand] : $rightOperand;
+		return sprintf('%s %s %s = %d', $leftOperand, $this->symbol[$operator], $rightOperand, $result);	
+	}
+
+	private function calculate($operator, $leftOperand, $rightOperand) {
+		switch ($operator) {
+			case 1: return $leftOperand + $rightOperand;
+			case 2: return $leftOperand * $rightOperand;
+			case 3: return $leftOperand - $rightOperand;
+		}	
 	}
 }
--- CaptchaTest.php
+++ CaptchaTest.php
@@ -1,28 +1,43 @@
 <?php 
 require_once "Captcha.php";
 class CaptchaTest extends PHPUnit_Framework_TestCase {
+
+	private $captcha;
+	private $randomizer;
+
+	function setUp() {
+		$this->captcha = new Captcha();
+	}
+
 	function testPatternOneOperatorPlus(){
-		$this->assertEquals('One + 1 = 2',numberToString(1,1,1,1));
+		$this->assertEquals('One + 1 = 2',$this->captcha->getCaptcha(1,1,1,1));
 	}
 
 	function testPatternOneOperatorMultiply(){
-		$this->assertEquals('One * 1 = 1',numberToString(1,1,2,1));
+		$this->assertEquals('One * 1 = 1',$this->captcha->getCaptcha(1,1,2,1));
 	}
 
 	function testPatternOneOperatorMinus(){
-		$this->assertEquals('Nine - 1 = 8',numberToString(1,9,3,1));
+		$this->assertEquals('Nine - 1 = 8',$this->captcha->getCaptcha(1,9,3,1));
 	}
 
 	function testPatternTwoOperatorPlus(){
-		$this->assertEquals('1 + One = 2',numberToString(2,1,1,1));
+		$this->assertEquals('1 + One = 2',$this->captcha->getCaptcha(2,1,1,1));
 	}
-	
+
 	function testPatternTwoOperatorMultiply(){
-		$this->assertEquals('1 * One = 1',numberToString(2,1,2,1));
+		$this->assertEquals('1 * One = 1',$this->captcha->getCaptcha(2,1,2,1));
 	}
 
 
 	function testPatternTwoOperatorMinus(){
-		$this->assertEquals('1 - One = 0',numberToString(2,1,3,1));
+		$this->assertEquals('1 - One = 0',$this->captcha->getCaptcha(2,1,3,1));
 	}
+
+	// function testGetCaptcha() {
+	// 	$service = new CaptchaService();
+	// 	$captcha = $service->getCaptcha;
+
+	// 	$this->assertEq("One + 1 = 2", $captcha->toString());
+	// }
 }
